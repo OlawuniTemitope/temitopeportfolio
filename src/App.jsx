24a5da0ceec26caacc7ee04e59a48461c7c9c4b0
Temitope Olawuni <olawunitemitope68@gmail.com>
2025-05-19 18:12:22 +0330

    Editing lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,37 @@
-// import Hero from "./components/hero/Hero";
-// import Services from "./components/services/Services";
-// import Portfolio from "./components/portfolio/Portfolio";
-// import Contact from "./components/contact/Contact";
+import Hero from "./components/hero/Hero";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
 
-import { lazy, Suspense } from "react";
+// import { lazy, Suspense } from "react";
 
 
-const Hero = lazy(() => import("./components/hero/Hero"));
-const Services = lazy(() => import("./components/services/Services"));
-const Portfolio = lazy(() => import("./components/portfolio/Portfolio"));
-const Contact = lazy(() => import("./components/contact/Contact"));
+// const Hero = lazy(() => import("./components/hero/Hero"));
+// const Services = lazy(() => import("./components/services/Services"));
+// const Portfolio = lazy(() => import("./components/portfolio/Portfolio"));
+// const Contact = lazy(() => import("./components/contact/Contact"));
 
 const App = () => {
   return (
     <div className="container">
-      <Suspense fallback={"loading..."}>
+      {/* <Suspense fallback={"loading..."}> */}
           <section id="#home">
             <Hero />
           </section>
-      </Suspense>
-      <Suspense >
+      {/* </Suspense> */}
+      {/* <Suspense > */}
           <section id="#services">
             <Services />
           </section>{" "}
-      </Suspense>
-      <Suspense >
+      {/* </Suspense> */}
+      {/* <Suspense > */}
           <Portfolio />
-      </Suspense>
-      <Suspense >
+      {/* </Suspense> */}
+      {/* <Suspense > */}
           <section id="#contact">
             <Contact />
           </section>{" "}
-      </Suspense>
+      {/* </Suspense> */}
     </div>
   );
 };
